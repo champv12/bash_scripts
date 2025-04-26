6996ce9082722d6e938cee0a1287756097d8ab79
Abuzar Haneefi <kittypaws875@gmail.com>
2025-04-26 11:56:35 +0530

    wrote the script

---
+++ remove_x_characters.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Number of characters to remove
+X = "$17"
+
+# Directory to search in
+path = "~/Downloads/AWS\ SAA-03\ Courses"
+
+# Check if the number of characters is provided
+if [[  -z "$X" ]]; then
+  echo "Usage: $0 <number_of_characters>"
+  exit 1
+fi
+
+# loop through all files in the current directory
+for file in "$path"/*; do
+    # checks if the file is a regular file
+    if [[ -f "$files" ]]; then
+        base="${file%.*}"
+        ext="${file##*.}"
+        new_base+="${base:0:-$X}" # removes the last X characters
+        if [[ "base" != "$new_base" ]]; then
+            mv "$file" "$new_base.$ext"
+        else
+            mv "$file" "$new_base"
+        fi
+        echo "Renamed $file to $new_base.$ext"
+    fi
+done
